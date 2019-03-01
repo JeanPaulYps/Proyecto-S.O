19eefaa2b0b4876ab30827da01e750a4b37484cb
Jean Paul <yjeanpaul@gmail.com>
2019-03-01 09:46:14 -0500

    Ya se puede navegar entre carpetas, falta solucionar bug de listar los directorios cuando cambia el link

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 from flask import Flask, render_template
+from pathlib import Path
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/<directorio>')
+def hello_world(directorio):
     direccion = os.getcwd()
-    direccion = os.chdir("..")
+    if not os.path.isfile(directorio):
+        direccion = os.chdir(directorio)
     archivos = os.listdir(direccion)
     directorios = []
     arch = []
-    for archivo in os.listdir(direccion):
+    for archivo in archivos:
         if os.path.isfile(archivo):
             arch.append(archivo)
         else:
             directorios.append(archivo)
-    
+    print(os.getcwd())
     print(arch)
     print(directorios)
+    return render_template('main.html', arch = arch, directorios = directorios)
+
+@app.route('/')
+def inicio():
+    os.chdir( str(Path.home()))
+    direccion =  str(Path.home())
+    archivos = os.listdir(direccion)
+    directorios = []
+    arch = []
+    for archivo in archivos:
+        if os.path.isfile(archivo):
+            arch.append(archivo)
+        else:
+            directorios.append(archivo)    
+    print(os.getcwd())
     return render_template('main.html', arch = arch, directorios = directorios)
